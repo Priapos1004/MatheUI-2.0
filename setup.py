deb1633501667b7c6996846f0cab8052070e7b0d
Samuel Brinkmann <samuel@brinkmann-familie.de>
2022-10-31 09:32:31 +0100

    Update publish-to-test-pypi.yml

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="matheUI_backend",
-    version="0.1.0",
+    version="0.1.1",
     description="little funny games package created by Samuel Brinkmann",
     long_description=long_description,
     long_description_content_type='text/markdown',
